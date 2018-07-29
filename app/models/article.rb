f388c8e1000914bb23e47957420eae3a2a4df83c
Sergey Antonov <33006817+sergeyantonov1@users.noreply.github.com>
2018-07-29 22:02:21 +0300

    Add searching by title or text of article and add pagination (#10)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,15 @@
 class Article < ApplicationRecord
+  include PgSearch
+
+  paginates_per 10
+
   has_many :comments
 
   belongs_to :user, inverse_of: :articles
 
   validates :title, :text, presence: true
+
+  pg_search_scope :search_by_title_or_text,
+    against: %i[title text],
+    using: { tsearch: { prefix: true } }
 end

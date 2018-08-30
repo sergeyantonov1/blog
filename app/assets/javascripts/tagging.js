d59b2a577be46069388d2fc2dde0196f7c219fee
Sergey Antonov <sergey.antonov@flatstack.com>
2018-08-30 15:02:59 +0400

    Change js and add css

--- app/assets/javascripts/tagging.js
+++ app/assets/javascripts/tagging.js
@@ -1,23 +1,25 @@
-var availableTags = []
+if(document.getElementById("tags")){
+  var availableTags = ["flatstack", "rails", "ruby"]
 
-$.ajax({
-  url: "/tags",
-  type: "get",
-  contentType: "application/json",
-  dataType: "json",
+  $.ajax({
+    url: "/tags",
+    type: "get",
+    contentType: "application/json",
+    dataType: "json",
 
-  success: function (result) {
-    for(var i = 0; i < result.length; i++)
-      availableTags[i] = result[i]
-  }
-});
-
-$(document).ready(function () {
-  $("#tags").tagit({
-    fieldName: "article[all_tags]",
-    singleField: true,
-    availableTags: availableTags,
-    allowDuplicates: false,
-    placeholderText: "Tags"
+    success: function (result) {
+      for(var i = 0; i < result.length; i++)
+        availableTags[i] = result[i]
+    }
   });
-});
+
+  $(document).ready(function () {
+    $("#tags").tagit({
+      fieldName: "article[all_tags]",
+      singleField: true,
+      availableTags: availableTags,
+      allowDuplicates: false,
+      placeholderText: "Tags"
+    });
+  });
+};
